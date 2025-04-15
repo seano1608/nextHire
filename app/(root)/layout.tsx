8b862b5f5e0e5b2ce4a413dddfc735cbc3ea07ed
Sean Thomson <seantomo1680@gmail.com>
2025-04-15 19:25:00 +0200

    Completed home page

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,16 @@
-import React, {ReactNode} from 'react';
+import React, { ReactNode } from "react";
+import Link from "next/link";
+import Image from "next/image";
 
-const Layout = ({children}: {children: ReactNode}) => {
+const Layout = ({ children }: { children: ReactNode }) => {
     return (
-        <div>
+        <div className="root-layout">
+            <nav>
+                <Link href="/" className="flex items-center gap-2">
+                    <Image src="/logo.svg" alt="Logo" width={38} height={32} />
+                    <h2 className="text-primary-100">NextHire</h2>
+                </Link>
+            </nav>
             {children}
         </div>
     );
